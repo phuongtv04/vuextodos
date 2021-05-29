1a7c9851d2c8b5f8ffaf7d0827a83e2723cce5e9
phuongtv04 <4hthang4@gmail.com>
2021-05-29 17:31:00 +0700

    VuexTodos

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="navbar">
+      <h1>My Vuex App</h1>
+      <ul>
+          <li>Home</li>
+          <li>About</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Navbar'
+}
+</script>
+
+<style>
+  .navbar {
+ padding: 5px;
+ text-align: center;
+ background: rgb(240, 240, 240);
+ color: black;
+}
+
+.navbar ul {
+ display: flex;
+ justify-content: space-around;
+ align-items: center;
+ list-style: none;
+ padding: 0;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import todos from './modules/todos';
+
+Vue.use(Vuex);
+
+const storeData = {
+    modules: {
+        todos
+    }
+}
+
+const store = new Vuex.Store(storeData)
+
+export default store
